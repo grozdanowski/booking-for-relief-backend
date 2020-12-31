686039267bfd7ee14a8dab13dd0be7df7717e78b
Matej Grozdanović <grozdanowski@gmail.com>
2020-12-31 11:07:08 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a873494cbc26e0dc00b4bb4906ee395'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'db688f8f-0d10-46f0-91a5-3cc269a35edb'
+};
