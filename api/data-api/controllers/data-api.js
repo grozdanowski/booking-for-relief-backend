801994858c755f512a98acf9541671f4cde53f97
Matej Grozdanović <grozdanowski@gmail.com>
2021-01-09 19:24:38 +0100

    small inline comments

--- api/data-api/controllers/data-api.js
+++ api/data-api/controllers/data-api.js
@@ -3,16 +3,19 @@ module.exports = {
   getLatest: async ctx => {
     const item = ctx.request.body;
     try {
+      // get all necessary items
       const aidRequests = await strapi.query('aid-request').find({ _limit: 20, _sort: 'id:desc', fulfilled: false })
       const aidCollections = await strapi.query('aid-collection').find({ _limit: 20, _sort: 'id:desc', fulfilled: false })
       const accommodations = await strapi.query('accommodation').find({ _limit: 20, _sort: 'id:desc', fulfilled: false })
       const transports = await strapi.query('transport').find({ _limit: 20, _sort: 'id:desc', fulfilled: false })
       const itemTags = await strapi.query('item-tag').find({ _limit: 20, _sort: 'tag' })
+      // remove unwanted fields
       aidRequests.forEach(function(item){ delete item.created_by; delete item.updated_by });
       aidCollections.forEach(function(item){ delete item.created_by; delete item.updated_by });
       accommodations.forEach(function(item){ delete item.created_by; delete item.updated_by });
       transports.forEach(function(item){ delete item.created_by; delete item.updated_by });
       itemTags.forEach(function(item){ delete item.created_by; delete item.updated_by });
+      // return
       ctx.send({
         'aidRequests': aidRequests,
         'aidCollections': aidCollections,
